c1129f982503cb63af3b2d9413c221ccefc06fa5
Caio-Gomes2007 <caiofgomesrj@gmail.com>
2024-10-16 20:24:45 -0300

    removendo arquivos desnecessarios

--- phpMorto/pegarDados.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include("connect.php");
-include("user.php");
-$jesus = new User("jesus", "jesus@gmail.com", "fdssenha");
-$jesus->cadastrar($conn);
-$id = $jesus->get_id("jesus");
-echo "foi?";
-$jesus->get_name($conn, $id);
